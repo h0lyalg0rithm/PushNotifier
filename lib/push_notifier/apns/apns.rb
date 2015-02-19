76d9144a54034320f5da88b4c12db811c3e675f8
Suraj Shirvankar <surajshirvankar@gmail.com>
2015-02-20 01:44:47 +0400

    URLS

--- lib/push_notifier/apns/apns.rb
+++ lib/push_notifier/apns/apns.rb
@@ -1,7 +1,11 @@
 module PushNotifier
   module APNS
     DEVELOPMENT_URL = 'gateway.sandbox.push.apple.com'
+    DEVELOPMENT_FEEDBACK_URL = 'feedback.sandbox.push.apple.com'
+
+    PRODUCTION_FEEDBACK_URL = 'feedback.push.apple.com'
     PRODUCTION_URL = 'gateway.push.apple.com'
+
 
 
   end
--- spec/push_notifier_spec.rb
+++ spec/push_notifier_spec.rb
@@ -9,5 +9,11 @@ describe PushNotifier do
     it 'should exist' do
       expect(defined?(PushNotifier::APNS)).not_to eq nil
     end
+    it 'have initial urls' do
+      expect(PushNotifier::APNS::DEVELOPMENT_URL).to match('sandbox')
+      expect(PushNotifier::APNS::DEVELOPMENT_FEEDBACK_URL).to match('feedback')
+      expect(PushNotifier::APNS::PRODUCTION_FEEDBACK_URL).to match('feedback')
+      expect(PushNotifier::APNS::PRODUCTION_URL).to match('push')
+    end
   end
 end
