60ccc80c224a6ad3be8ca7960af9568d82de95b2
Suraj Shirvankar <surajshirvankar@gmail.com>
2015-02-25 01:24:55 +0400

    Custom notifications

--- lib/push_notifier/apns/notification.rb
+++ lib/push_notifier/apns/notification.rb
@@ -3,34 +3,47 @@ require 'json'
 module PushNotifier
   class APNS
     class Notification
-      def initialize(token, message, *options)
-        @device_token = clean_token token
-        # @options = default_options.merge(options)
+      attr_accessor :options,:token
+      def initialize(token, message, options={})
+        default_options
+        dynamic_accessor options
+        @token = clean_token token
+        @alert = message
+        @custom_data = options.reject { |x,y| allowed_attrs.include? x }
       end
+
       def default_options
-        {
-            alert: 'true',
-            badge: 1,
-            sound: 'default',
-            priority: 5
-        }
+        @alert = ''
       end
+
       def clean_token(token)
         [token.gsub!(/[<|>|\s]/,'')].pack('H*')
       end
+
       def data_message
-        aps = {'aps'=> {} }
-        aps['aps']['alert'] = 'test'
-        aps['aps']['badge'] = 1
-        aps['aps']['sound'] = true
-        aps.to_json.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
+        aps = allowed_attrs.inject({}) do |acc,x|
+          acc[x.to_s] = self.send(x) unless self.send(x).nil?
+          acc
+        end
+        custom_data = aps.delete('custom_data')
+        custom_data[:aps] = aps
+        puts custom_data
+        custom_data.to_json#.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
       end
-      def frame_data
-        # [@device_token]
+
+      def allowed_attrs
+        [:alert, :badge, :category, :content_available, :sound, :custom_data, :priority]
+      end
+
+      def dynamic_accessor(option)
+        allowed_attrs.each do |x|
+          singleton_class.class_eval do; attr_accessor "#{x}"; end
+          self.send("#{x}=",option[x])
+        end
       end
       def payload
         dm = data_message
-        [0, 0, 32, @device_token, 0, dm.bytesize, dm].pack("ccca*cca*")
+        [0, 0, 32, @token, 0, dm.bytesize, dm].pack("ccca*cca*")
       end
     end
   end
--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PushNotifier::APNS::Notification do
-  it 'creates notification' do
-    # @apns = PushNotifier::APNS::Notification.new '1234','message'
+  it 'sets default value ' do
+    @apns = PushNotifier::APNS::Notification.new '1234','message'
+    expect(@apns.data_message).to eq 1
   end
   it 'cleans device token' do
-
+    @apns = PushNotifier::APNS::Notification.new '1234', 'message'
+    expect(@apns.alert).to eq 'message'
+  end
+  it 'saves other info as custom data' do
+    @apns = PushNotifier::APNS::Notification.new '1234', 'message', data: 'test', sound: true
+    expect(@apns.custom_data[:data]).to eq 'test'
+    expect(@apns.custom_data[:sound]).to eq nil
+  end
+  it 'creats message data' do
+    @apns = PushNotifier::APNS::Notification.new '1234', 'message', data: 'test', sound: true
+    expect(JSON.parse(@apns.data_message)["aps"]["sound"]).to eq true
   end
 end
 
